e196dbafa56d2c0b05e14d325dc8633a17aa670f
seal0207 <50684179+seal0207@users.noreply.github.com>
2021-03-25 09:39:10 +0800

    修正定时任务重启

--- realm.sh
+++ realm.sh
@@ -448,7 +448,7 @@ Time_Task(){
   echo -e "请设置每多少分钟重启Realm任务"	
   read -p "请设置分钟数:
   " type_m
-  echo "*/$type_m * * * * systemctl restart realm" >> /var/spool/cron/crontabs/root
+  echo "*/$type_m * * * * /usr/bin/systemctl restart realm" >> /var/spool/cron/crontabs/root
   sync /var/spool/cron/crontabs/root
   systemctl restart cron 
 	;;
@@ -456,7 +456,7 @@ Time_Task(){
   echo -e "请设置每多少小时重启Realm任务"	
   read -p "请设置小时数:
   " type_h
-  echo "* */$type_h * * * systemctl restart realm" >> /var/spool/cron/crontabs/root
+  echo "* */$type_h * * * /usr/bin/systemctl restart realm" >> /var/spool/cron/crontabs/root
   sync /var/spool/cron/crontabs/root
   systemctl restart cron
 	;;
@@ -464,7 +464,7 @@ Time_Task(){
   echo -e "请设置每多少分钟重启Realm任务"	
   read -p "请设置天数:
   " type_m
-  echo "* * */$type_d * * systemctl restart realm" >> /var/spool/cron/crontabs/root
+  echo "* * */$type_d * * /usr/bin/systemctl restart realm" >> /var/spool/cron/crontabs/root
   sync /var/spool/cron/crontabs/root
   systemctl restart cron
 	;;
