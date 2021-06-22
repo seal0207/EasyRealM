4349d595c9107899324c56f0f1b8076e77ef5325
seal0207 <50684179+seal0207@users.noreply.github.com>
2021-06-22 23:27:58 +0800

    更新realm为修复版1.2.2下载地址
    更新realm为修复版1.2.2下载地址

--- realm.sh
+++ realm.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 clear
 
-sh_ver="1.0.6"
+sh_ver="1.0.7"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
